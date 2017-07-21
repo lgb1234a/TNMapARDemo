83962432490f9cff28815005a2b5bde585a88d74
勒 <legongbiao@tuniu.com>
2017-07-21 14:56:18 +0800

    init

--- TNMapDemo/ViewController.m
+++ TNMapDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <MAMapKit/MAMapKit.h>
 
 @interface ViewController ()
 
@@ -17,6 +18,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    ///地图需要v4.5.0及以上版本才必须要打开此选项（v4.5.0以下版本，需要手动配置info.plist）
+    [AMapServices sharedServices].enableHTTPS = YES;
+    
+    ///初始化地图
+    MAMapView *_mapView = [[MAMapView alloc] initWithFrame:self.view.bounds];
+    
+    ///把地图添加至view
+    [self.view addSubview:_mapView];
+    
+    ///如果您需要进入地图就显示定位小蓝点，则需要下面两行代码
+    _mapView.showsUserLocation = YES;
+    _mapView.userTrackingMode = MAUserTrackingModeFollow;
 }
 
 
