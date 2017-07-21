59603fd36c123b690b56936769d1b39704dd3445
勒 <legongbiao@tuniu.com>
2017-07-21 17:56:50 +0800

    step 1

--- TNMapDemo/ViewController.m
+++ TNMapDemo/ViewController.m
@@ -9,7 +9,9 @@
 #import "ViewController.h"
 #import <MAMapKit/MAMapKit.h>
 
-@interface ViewController ()
+@interface ViewController () <MAMapViewDelegate>
+
+@property (nonatomic, strong) MAMapView *mapView;
 
 @end
 
@@ -22,15 +24,92 @@
     [AMapServices sharedServices].enableHTTPS = YES;
     
     ///初始化地图
-    MAMapView *_mapView = [[MAMapView alloc] initWithFrame:self.view.bounds];
+    _mapView = [[MAMapView alloc] initWithFrame:self.view.bounds];
+    _mapView.delegate = self;
     
     ///把地图添加至view
     [self.view addSubview:_mapView];
     
     ///如果您需要进入地图就显示定位小蓝点，则需要下面两行代码
+    [_mapView setZoomLevel:14 animated:YES];
     _mapView.showsUserLocation = YES;
-    _mapView.userTrackingMode = MAUserTrackingModeFollow;
+    _mapView.userTrackingMode = MAUserTrackingModeFollowWithHeading;
 }
+
+- (void)addAnnotations
+{
+    MAPointAnnotation *pointAnnotation = [[MAPointAnnotation alloc] init];
+    pointAnnotation.coordinate = CLLocationCoordinate2DMake(32.084861, 118.887380);
+    pointAnnotation.title = @"方恒国际";
+    pointAnnotation.subtitle = @"阜通东大街6号";
+    
+    [_mapView addAnnotation:pointAnnotation];
+    
+//    [_mapView addAnnotations:@[]];
+}
+
+/**
+ * @brief 地图加载成功
+ * @param mapView 地图View
+ */
+- (void)mapViewDidFinishLoadingMap:(MAMapView *)mapView
+{
+    
+}
+
+- (void)mapViewDidStopLocatingUser:(MAMapView *)mapView
+{
+    
+}
+
+/**
+ * @brief 当mapView新添加annotation views时，调用此接口
+ * @param mapView 地图View
+ * @param views 新添加的annotation views
+ */
+- (void)mapView:(MAMapView *)mapView didAddAnnotationViews:(NSArray *)views
+{
+    //构造圆
+    MACircle *circle = [MACircle circleWithCenterCoordinate:_mapView.userLocation.location.coordinate radius:1000];
+    
+    NSLog(@"%lf ---  %lf", _mapView.userLocation.location.coordinate.longitude, _mapView.userLocation.location.coordinate.latitude);
+    //在地图上添加圆
+    [_mapView addOverlay: circle];
+    
+//    [self addAnnotations];
+}
+
+/**
+ * @brief 根据overlay生成对应的Renderer
+ * @param mapView 地图View
+ * @param overlay 指定的overlay
+ * @return 生成的覆盖物Renderer
+ */
+- (MAOverlayRenderer *)mapView:(MAMapView *)mapView rendererForOverlay:(id <MAOverlay>)overlay
+{
+    NSLog(@"%lf ---  %lf", overlay.coordinate.longitude, overlay.coordinate.latitude);
+    if ([overlay isKindOfClass:[MACircle class]])
+    {
+        MACircleRenderer *circleRenderer = [[MACircleRenderer alloc] initWithCircle:overlay];
+        
+        circleRenderer.lineWidth    = 10.f;
+        circleRenderer.strokeColor  = [UIColor colorWithRed:0.1 green:0.4 blue:1.0 alpha:0.6];
+        circleRenderer.fillColor    = [UIColor colorWithRed:0.1 green:0.4 blue:0.7 alpha:0.5];
+        return circleRenderer;
+    }
+    return nil;
+}
+
+/**
+ * @brief 根据anntation生成对应的View
+ * @param mapView 地图View
+ * @param annotation 指定的标注
+ * @return 生成的标注View
+ */
+//- (MAAnnotationView *)mapView:(MAMapView *)mapView viewForAnnotation:(id<MAAnnotation>)annotation
+//{
+//    
+//}
 
 
 - (void)didReceiveMemoryWarning {
