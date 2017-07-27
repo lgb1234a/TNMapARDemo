afd1476172acbff2943b6ea00831ad836f4cfc52
勒 <legongbiao@tuniu.com>
2017-07-27 10:50:31 +0800

    [modify]获取需要的设备数据

--- TNMapDemo/TNRadarView.m
+++ TNMapDemo/TNRadarView.m
@@ -18,6 +18,7 @@
 
 @end
 
+#define DEGREES(d) 180 / M_PI * d
 @implementation TNRadarView
 
 - (instancetype)initWithFrame:(CGRect)frame
@@ -58,20 +59,7 @@
 
 - (void)updateRadarStatuWithDeviceMotion:(CMDeviceMotion *)motion
 {
-    //获取这个然后使用这个角度进行view旋转，可以实现view保持水平的效果，设置一个图片可以测试
-    double rotation = atan2(motion.gravity.x, motion.gravity.y) - M_PI;
-    //            weakSelf.arImageView.transform = CGAffineTransformMakeRotation(rotation);
-    
-    //2. Gravity 获取手机的重力值在各个方向上的分量，根据这个就可以获得手机的空间位置，倾斜角度等
-    double gravityX = motion.gravity.x;
-    double gravityY = motion.gravity.y;
-    double gravityZ = motion.gravity.z;
-    
-    //获取手机的倾斜角度(zTheta是手机与水平面的夹角， xyTheta是手机绕自身旋转的角度)：
-    double zTheta = atan2(gravityZ,sqrtf(gravityX*gravityX+gravityY*gravityY))/M_PI*180.0;
-    double xyTheta = atan2(gravityX,gravityY)/M_PI*180.0;
-    
-    NSLog(@"旋转角度：%.4f 水平倾角：%.4f，旋转角度：%.4f", motion.attitude.roll, zTheta, xyTheta);
+    NSLog(@"roll角度：%.4f, pitch角度：%.4f, yaw角度：%.4f", DEGREES(motion.attitude.roll), DEGREES(motion.attitude.pitch), DEGREES(motion.attitude.yaw));
 }
 
 
